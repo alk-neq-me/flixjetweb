1212fe15c9d2550ab69fdb55ec2fd960e8315a43
marco <toyko20012gmail.com>
2023-08-31 16:21:21 +0630

    infinity scroll

--- src/components/Movie/MovieList.tsx
+++ src/components/Movie/MovieList.tsx
@@ -3,7 +3,7 @@
 import Poster from "@/components/Poster";
 import { fetchPopularMovies } from "@/lib/movies"
 import { useInfiniteQuery } from "@tanstack/react-query"
-import { useCallback, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function ListMovies() {
   const {
@@ -17,28 +17,23 @@ function ListMovies() {
     getNextPageParam: lastPage => lastPage.nextPage,
   });
 
-  const handleScroll = useCallback(() => {
-    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || isLoading) {
-      return;
-    }
-    fetchNextPage();
-  }, [fetchNextPage])
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [isLoading])
-
   if (isLoading) return <h1>Loading...</h1>
   if (isError) return <h1>Error...</h1>
 
   return <div className="sm:px-2 md:px-4 space-y-8">
     {data?.pages?.map((movies, index) => (
-      <div key={index} className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 sm:gap-2">
+      <InfiniteScroll 
+        key={index} 
+        dataLength={movies.data.length}
+        next={fetchNextPage}
+        hasMore={!!movies.nextPage}
+        loader={<p>Loading...</p>}
+        endMessage={<p>No More data</p>}
+        className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 sm:gap-2">
         {movies.data?.map(movie => (
           <Poster key={`${movie.id}`} uri={movie.poster_path} tmdbid={movie.id.toString()} />
         ))}
-      </div>
+      </InfiniteScroll>
     ))}
   </div>
 }
